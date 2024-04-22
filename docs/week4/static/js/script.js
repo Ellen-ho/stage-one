6c48bd094bcdeb69275c9298abe7502b2370d0b7
Ellen <ellen31725@gmail.com>
2024-04-23 01:17:06 +0800

    feat: add script js file

---
+++ docs/week4/static/js/script.js
@@ -0,0 +1,32 @@
+function addSignInFormEventListener() {
+    const signInForm = document.getElementById("signin-form");
+    const checkbox = document.getElementById("checkbox");
+
+    signInForm.onsubmit = function(event) {
+        if (!checkbox || !checkbox.checked) {
+            alert("Please check the checkbox first");
+            event.preventDefault();
+        }
+    };
+
+}
+
+function addCalculateFormEventListener() {
+    const calculateBtn = document.getElementById('calculate');
+
+    calculateBtn.addEventListener('click', function() {
+        const number = parseInt(document.getElementById('inputint').value, 10); 
+        if (!isNaN(number) && number > 0) {
+            window.location.href = `/square/${number}`; 
+        } else {
+            alert("Please enter a valid positive integer!");
+        }
+    })
+}
+
+function init() {
+    addSignInFormEventListener();
+    addCalculateFormEventListener();
+}
+
+window.onload = init;
